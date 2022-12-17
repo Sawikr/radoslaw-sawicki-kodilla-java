785afde8f6aa3ad8b5e03a12987e9986c53c435c
Radoslaw Sawicki <sawikr@op.pl>
2022-12-17 19:38:30 +0100

    Correction 6.7 Additional task

--- kodilla-testing/src/main/java/com/kodilla/testing/statistics/CalculateAdvStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/statistics/CalculateAdvStatistics.java
@@ -3,58 +3,71 @@ package com.kodilla.testing.statistics;
 import java.util.List;
 
 
-public class CalculateAdvStatistics{
+public class CalculateAdvStatistics implements Statistics{
 
     List<String> usersNames;
-    private String usersCount;
+    private int usersCount;
+    private int usersCountAdv;
     private int postsCount;
     private int commentsCount;
     private int averagePostsCountPerUser;
     private int averageCommentsCountPerUser;
     private int averageCommentsCountPerPost;
-    private int usersCounter;
 
-    private Statistics statistics;
+    Statistics statistics;
 
     public CalculateAdvStatistics(Statistics statistics) {
         this.statistics = statistics;
     }
 
-    public CalculateAdvStatistics(String usersCount, int postsCount, int commentsCount) {
+    public CalculateAdvStatistics(int usersCount, int postsCount, int commentsCount) {
         this.usersCount = usersCount;
         this.postsCount = postsCount;
         this.commentsCount = commentsCount;
     }
 
-    List<String> usersNames(){
+    public CalculateAdvStatistics(List<String> usersNames, int usersCount, int postsCount, int commentsCount) {
+        this.usersNames = usersNames;
+        this.usersCount = usersCount;
+        this.postsCount = postsCount;
+        this.commentsCount = commentsCount;
+    }
+
+    public CalculateAdvStatistics(List<String> usersNames) {
+        this.usersNames = usersNames;
+    }
+
+    @Override
+    public List<String> usersNames(){
         return usersNames;
     }
 
-    public int commentsCount() {
-        return commentsCount;
+    @Override
+    public int postsCount() {
+        return postsCount;
     }
 
-    public int getPostsCount() {
-        return postsCount;
+    @Override
+    public int commentsCount() {
+        return commentsCount;
     }
 
     void calculateAdvStatistics(Statistics statistics){
         postsCount = statistics.postsCount();
         commentsCount = statistics.commentsCount();
         usersNames = statistics.usersNames();
+        usersCountAdv = usersNames.size();
+        averageCommentsCountPerPost = 10;
+        averageCommentsCountPerUser = 11;
+        averagePostsCountPerUser = 12;
     }
 
     void showStatistics(){
-        System.out.println(" " + usersCount);
-        System.out.println(" " + postsCount);
-        System.out.println(" " + commentsCount);
-        System.out.println(" " + averagePostsCountPerUser);
-        System.out.println(" " + averageCommentsCountPerUser);
-        System.out.println(" " + averageCommentsCountPerPost);
+        System.out.println("UsersCount: " + usersCountAdv);
+        System.out.println("PostsCount: " + postsCount);
+        System.out.println("CommentsCount: " + commentsCount);
+        System.out.println("AveragePostsCountPerUser: " + averagePostsCountPerUser);
+        System.out.println("AverageCommentsCountPerUser: " + averageCommentsCountPerUser);
+        System.out.println("AverageCommentsCountPerPost: " + averageCommentsCountPerPost);
     }
-
-    public Integer postCount() {
-        return postsCount;
-    }
-
 }
