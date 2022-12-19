ff8c6284fadb9ec3281914824d531bf6beb5b283
Radoslaw Sawicki <sawikr@op.pl>
2022-12-19 21:44:35 +0100

    Correction 21: 6.7 Additional task

--- kodilla-testing/src/test/java/com/kodilla/testing/statistics/TestCalculateAdvStatistics.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/statistics/TestCalculateAdvStatistics.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -57,38 +59,59 @@ public class TestCalculateAdvStatistics{
     @DisplayName("Name of test: TestCalculateAdvStatistics")
     @Test
     void testCalculateAdvStatisticsPostsCount0() {
+        //Correct solution!
+
         //Given
-        CalculateAdvStatistics calculate = new CalculateAdvStatistics(statisticsMock);
+        //Mt: argument 'statistics' is not need!
+        //CalculateAdvStatistics calculate = new CalculateAdvStatistics(statisticsMock);
+        CalculateAdvStatistics calculate = new CalculateAdvStatistics();
         List<String> usersNames = new ArrayList<>();
-        CalculateAdvStatistics testCount = new CalculateAdvStatistics(usersNames,1, 0, 100);
+        //Not need
+        //CalculateAdvStatistics testCount = new CalculateAdvStatistics(usersNames,1, 0, 100);
 
         //When
+        //Mt: correction
+        when(statisticsMock.postsCount()).thenReturn(0);
+        when(statisticsMock.commentsCount()).thenReturn(0);
+        when(statisticsMock.usersNames()).thenReturn(usersNames);
+
         calculate.calculateAdvStatistics(statisticsMock);
 
         //Then
-        assertEquals(0, testCount.postsCount());
+        assertEquals(0, calculate.getAverageCommentsCountPerPost());
+        assertEquals(0, calculate.getAveragePostsCountPerUser());
+        assertEquals(0, calculate.getAverageCommentsCountPerUser());
+
         calculate.showStatistics();
     }
 
     @Test
     void testCalculateAdvStatisticsPostsCount1000() {
         //Given
-        CalculateAdvStatistics calculate = new CalculateAdvStatistics(statisticsMock);
+        CalculateAdvStatistics calculate = new CalculateAdvStatistics();
         List<String> usersNames = new ArrayList<>();
-        CalculateAdvStatistics testCount = new CalculateAdvStatistics(usersNames,  1, 1000, 100);
+        CalculateAdvStatistics user1 = new CalculateAdvStatistics(usersNames,  0, 0, 0);
+        usersNames.add(String.valueOf(user1));
 
         //When
+        when(statisticsMock.postsCount()).thenReturn(1000);
+        when(statisticsMock.commentsCount()).thenReturn(1000);
+        when(statisticsMock.usersNames()).thenReturn(Collections.singletonList(usersNames.get(0)));
+
         calculate.calculateAdvStatistics(statisticsMock);
 
         //Then
-        assertEquals(1000, testCount.postsCount());
+        assertEquals(1, calculate.getAverageCommentsCountPerPost());
+        assertEquals(1000, calculate.getAveragePostsCountPerUser());
+        assertEquals(1000, calculate.getAverageCommentsCountPerUser());
+        //assertEquals(1000, testCount.postsCount());
         calculate.showStatistics();
     }
 
     @Test
     void testCalculateAdvStatisticsCommentsCount1000() {
         //Given
-        CalculateAdvStatistics calculate = new CalculateAdvStatistics(statisticsMock);
+        CalculateAdvStatistics calculate = new CalculateAdvStatistics();
         List<String> usersNames = new ArrayList<>();
         CalculateAdvStatistics testCount = new CalculateAdvStatistics(usersNames, 1, 1000, 0);
 
@@ -103,7 +126,7 @@ public class TestCalculateAdvStatistics{
     @Test
     void testCalculateAdvStatisticsFewerCommentsThanPosts() {
         //Given
-        CalculateAdvStatistics calculate = new CalculateAdvStatistics(statisticsMock);
+        CalculateAdvStatistics calculate = new CalculateAdvStatistics();
         List<String> usersNames = new ArrayList<>();
         CalculateAdvStatistics testCount = new CalculateAdvStatistics(usersNames, 1, 10, 9);
         int post = testCount.postsCount();
@@ -124,7 +147,7 @@ public class TestCalculateAdvStatistics{
     @Test
     void testCalculateAdvStatisticsFewerPostThanComments() {
         //Given
-        CalculateAdvStatistics calculate = new CalculateAdvStatistics(statisticsMock);
+        CalculateAdvStatistics calculate = new CalculateAdvStatistics();
         List<String> usersNames = new ArrayList<>();
         CalculateAdvStatistics testCount = new CalculateAdvStatistics(usersNames,1, 10, 11);
         int post = testCount.postsCount();
@@ -145,7 +168,7 @@ public class TestCalculateAdvStatistics{
     @Test
     void testCalculateAdvStatisticsUsers0Count() {
         //Given
-        CalculateAdvStatistics calculate = new CalculateAdvStatistics(statisticsMock);
+        CalculateAdvStatistics calculate = new CalculateAdvStatistics();
 
         List<String> listOfUsers = new ArrayList<>();
         CalculateAdvStatistics user1 = new CalculateAdvStatistics(listOfUsers,1, 1000, 0);
@@ -165,7 +188,7 @@ public class TestCalculateAdvStatistics{
     @Test
     void testCalculateAdvStatisticsUsers100Count() {
         //Given
-        CalculateAdvStatistics calculate = new CalculateAdvStatistics(statisticsMock);
+        CalculateAdvStatistics calculate = new CalculateAdvStatistics();
         List<String> resultListOf100Users = generateListOfNUsers(100);
 
         //When
