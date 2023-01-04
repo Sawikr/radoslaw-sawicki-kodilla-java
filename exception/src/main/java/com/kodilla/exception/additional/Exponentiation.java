ec054c67be20cdd3fe82fbc114911be751faa0b4
Radoslaw Sawicki <sawikr@op.pl>
2023-01-05 00:17:56 +0100

    8.2 Additional: Exponentiation

---
+++ exception/src/main/java/com/kodilla/exception/additional/Exponentiation.java
@@ -0,0 +1,30 @@
+package com.kodilla.exception.additional;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Exponentiation {
+
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        long a, b, c;
+        long result = 1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number (base of power):");
+        a = scanner.nextInt();
+        System.out.println("Enter number (exponent):");
+        b = scanner.nextInt();
+        c = b;
+
+        //Method: exponentiation
+        while(b > 0)
+        {
+            result *= a;
+            b--;
+        }
+
+        System.out.println("The " + a + " raised to the " + c + " is: " + result);
+    }
+}
