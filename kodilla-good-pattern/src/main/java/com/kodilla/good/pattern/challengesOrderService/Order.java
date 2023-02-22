c93039c0aaef4ae21ff524562194a8c30e1d7b64
Radosław Sawicki <sawikr@op.pl>
2023-02-22 23:13:24 +0100

    13.2 Task: Update orderService

--- kodilla-good-pattern/src/main/java/com/kodilla/good/pattern/challengesOrderService/Order.java
+++ kodilla-good-pattern/src/main/java/com/kodilla/good/pattern/challengesOrderService/Order.java
@@ -1,10 +1,7 @@
 package com.kodilla.good.pattern.challengesOrderService;
 
-<<<<<<< HEAD
 import com.kodilla.good.pattern.challengesOrderService.anotherVersion.ProductOne;
 import com.kodilla.good.pattern.challengesOrderService.anotherVersion.ProductTwo;
-=======
->>>>>>> origin/master
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,19 +10,11 @@ public class Order {
     private final List<String> newList = new ArrayList<>();
 
     public List<String> order() {
-<<<<<<< HEAD
         ProductOne productOne = new ProductOne("Computer-AXN55G");
         newList.add(String.valueOf(productOne.getListProduct(productOne)));
 
         ProductTwo productTwo = new ProductTwo("Computer-GD2");
         newList.add(String.valueOf(productTwo.getListProduct(productTwo)));
-=======
-        ProductOne productOne = new ProductOne();
-        newList.add(String.valueOf(productOne.getListProduct("Computer-AXN55G")));
-
-        ProductTwo productTwo = new ProductTwo();
-        newList.add(String.valueOf(productTwo.getListProduct("Computer-GD2")));
->>>>>>> origin/master
 
         return newList;
     }
