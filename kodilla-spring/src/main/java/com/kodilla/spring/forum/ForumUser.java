53f3da3bcbb30bd1e135f7a0ae0a88e3730fa6b5
Radosław Sawicki <sawikr@op.pl>
2023-02-28 21:44:04 +0100

    14.1 Task: bean ForumUser

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+
+    public String username;
+
+    public ForumUser() {
+        username = "John Smith";
+    }
+}
