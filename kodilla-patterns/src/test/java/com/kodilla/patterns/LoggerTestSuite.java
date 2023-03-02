39e5ee2e816305744046cbb71a42de976266104e
Radosław Sawicki <sawikr@op.pl>
2023-03-02 23:27:22 +0100

    15.1 Task: singleton used to save logs of performed operations

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/LoggerTestSuite.java
@@ -0,0 +1,56 @@
+package com.kodilla.patterns;
+
+import com.kodilla.patterns.singleton.Logger;
+import com.kodilla.patterns.singleton.LoggerSingleton;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoggerTestSuite {
+
+    private static int testCounter = 0;
+
+    @BeforeEach
+    public void before() {
+        System.out.println("\nTest Case: begin!");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("Test Case: end!");
+    }
+
+    @BeforeEach
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @DisplayName("Name of test: loggerTestSuite")
+    @Test
+    void loggerTestSuite() {
+        //Given
+        Logger logger = new Logger();
+
+        //When
+        logger.log("New logger");
+
+        //Then
+        assertEquals("New logger", logger.getLastLog());
+    }
+
+    @DisplayName("Name of test: loggerTestSuiteSingleton")
+    @Test
+    void loggerTestSuiteSingleton() {
+        //Given
+        LoggerSingleton logger = LoggerSingleton.INSTANCE;
+
+        //When
+        logger.log("New loggerSingleton");
+
+        //Then
+        assertEquals("New loggerSingleton", logger.getLastLog());
+    }
+}
