128c66c6854e446a640d6c148ed98ae019a31615
Radosław Sawicki <sawikr@op.pl>
2023-03-05 22:40:01 +0100

    15.5 Task: Composing your own Big Mac

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,68 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BigmacTestSuite {
+
+    private static int testCounter = 0;
+
+    @BeforeEach
+    public void before() {
+        System.out.println("\nTest Case: begin!");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("Test Case: end!");
+    }
+
+    @BeforeEach
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @DisplayName("Name of test: testBigmacTestSuite")
+    @Test
+    void testBigmacTestSuite() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .getBun("MacBun")
+                .getBurgers(2)
+                .getSauce("Standard")
+                .getIngredients("Salad")
+                .getIngredients("Bacon")
+                .getIngredients("Cucumber")
+                .build();
+
+        System.out.println(bigmac);
+
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        Bigmac.BigmacBuilder bigmacBuilder = new Bigmac.BigmacBuilder();
+        Bigmac.BigmacBuilder howManyBurgers = bigmacBuilder.getBurgers(2);
+
+        //Then
+        assertEquals(3, howManyIngredients);
+        assertEquals(Bigmac.BigmacBuilder.class, howManyBurgers.getClass());
+    }
+
+    @DisplayName("Name of test: testBigmacTestSuiteIsCorrect")
+    @Test
+    void testBigmacTestSuiteIsCorrect() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .getBun("MacBun")
+                .getIngredients("Bacon")
+                .build();
+
+        System.out.println(bigmac);
+
+        //When & then
+        assertNotNull(bigmac);
+    }
+}
